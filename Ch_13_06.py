54a6d31e1fc64d8078148a1df2c57f31e18f1c47
pederseo <135082830+pederseo@users.noreply.github.com>
2024-06-13 22:32:55 -0400

    Update Ch_13_06.py

--- Ch_13_06.py
+++ Ch_13_06.py
@@ -1,1 +1,12 @@
 
+#Contar Vocales: Escribe un programa que cuente el número de vocales en una cadena dada.
+
+palabra = "challenge dia 3"
+vocales = "aeiou"
+cont_vocales = ""
+
+for p in palabra:
+    for v in vocales:
+        if v in p:
+            cont_vocales = cont_vocales+p
+print(f"la cantidad de vocales dentro de la frase: '{palabra}' es ({len(cont_vocales)})")
