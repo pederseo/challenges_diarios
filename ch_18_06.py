0356709e08a7cd8ff862d316f3b87c4275f4866b
pederseo <135082830+pederseo@users.noreply.github.com>
2024-06-18 11:38:45 -0400

    Update ch_18_06.py

--- ch_18_06.py
+++ ch_18_06.py
@@ -2,13 +2,9 @@
 # Conversión de Temperatura: Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit.
 
 def celsius_a_Fahrenheit(celsius):
-    fahrenheit = celsius - 32 * 5/9
+    fahrenheit = celsius * 9/5 + 32
     print(f'{int(celsius)} celsius = {int(fahrenheit)} fahrenheit')
 
-def fahrenheit_a_celsius(fahrenheit):
-    celsius = fahrenheit * 9/5 + 32
-    print(f'{int(fahrenheit)} fahrenheit = {int(celsius)} celsius')
+celsius_a_Fahrenheit(45)
 
-c = celsius_a_Fahrenheit(45)
-f = fahrenheit_a_celsius(113)
 
